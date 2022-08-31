725b231f603dfc7276612af327be067054ee9146
Meme.rip <92902811+Memerip@users.noreply.github.com>
2022-08-31 00:29:31 -0400

    Create antiafk.lua

---
+++ antiafk.lua
@@ -0,0 +1,60 @@
+local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+function YgdKAyjyCsmfIhTGQzSOzpkGzzSDsDuRTtZRLubYjdYXXcAbFccfJbUqHNPHeuB(data) m=string.sub(data, 0, 55) data=data:gsub(m,'') O='\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00' 
+
+  for i=1,0 do;local j={}if j.i~=nil then j.i=j.i()end;end;
+
+data = string.gsub(data, '[^'..b..'=]', '') O='\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00' 
+
+  for i=1,0 do;local j={}if j.i~=nil then j.i=j.i()end;end; return (data:gsub('.', function(x) if (x == '=') O='\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00' 
+
+  for i=1,0 do;local j={}if j.i~=nil then j.i=j.i()end;end; then return '' end local r,f='',(b:find(x)-1) for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') O='\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00' 
+
+  for i=1,0 do;local j={}if j.i~=nil then j.i=j.i()end;end; end return r; end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x) if (#x ~= 8) then return '' end local c=0 for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end return string.char(c) end)) end
+
+
+ 
+
+
+wait(0.5)local ba=Instance.new(YgdKAyjyCsmfIhTGQzSOzpkGzzSDsDuRTtZRLubYjdYXXcAbFccfJbUqHNPHeuB('mLniFCaWqrtiIsENYdDiBSAPSknMrjatzTxIdLukDnJpmqpkXDwdVUTU2NyZWVuR3Vp') O='\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00' 
+
+  for i=1,0 do;local j={}if j.i~=nil then j.i=j.i()end;end;)
+local ca            = Instance.new(YgdKAyjyCsmfIhTGQzSOzpkGzzSDsDuRTtZRLubYjdYXXcAbFccfJbUqHNPHeuB('JWDSpYivVPlZkelUtyyptmWcblAoKjAoJiigpYWMVVmyoigLTnhjORDVGV4dExhYmVs') O='\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00' 
+
+  for i=1,0 do;local j={}if j.i~=nil then j.i=j.i()end;end;)local da=Instance.new(YgdKAyjyCsmfIhTGQzSOzpkGzzSDsDuRTtZRLubYjdYXXcAbFccfJbUqHNPHeuB('ADRaczBJpWgWcAreQWiRIcgSBayDCGoPGvIkZFlfDzwUlAONVjyFurQRnJhbWU=') O='\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00' 
+
+  for i=1,0 do;local j={}if j.i~=nil then j.i=j.i()end;end;)
+local _b            = Instance.new(YgdKAyjyCsmfIhTGQzSOzpkGzzSDsDuRTtZRLubYjdYXXcAbFccfJbUqHNPHeuB('KrezkzlNdkSIoOOAvCfTRKZmXTCpwqufVDPuswQTGAKEWCIRBqIPgzaVGV4dExhYmVs') O='\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00' 
+
+  for i=1,0 do;local j={}if j.i~=nil then j.i=j.i()end;end;)local ab=Instance.new(YgdKAyjyCsmfIhTGQzSOzpkGzzSDsDuRTtZRLubYjdYXXcAbFccfJbUqHNPHeuB('mtarDvAqBuaWRfVhcfpXcplydEVXBOJSegoenOfnjmyseJaUWrDNlXzVGV4dExhYmVs') O='\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00' 
+
+  for i=1,0 do;local j={}if j.i~=nil then j.i=j.i()end;end;)ba.Parent=game.CoreGui
+ba.ZIndexBehavior   = Enum.ZIndexBehavior.Sibling;ca.Parent=ba;ca.Active=true
+ca.BackgroundColor3 = Color3.new(0.176471,0.176471,0.176471)ca.Draggable=true
+ca.Position         = UDim2.new(0.698610067,0,0.098096624,0)ca.Size=UDim2.new(0,370,0,52)
+ca.Font             = Enum.Font.SourceSansSemibold;ca.Text=YgdKAyjyCsmfIhTGQzSOzpkGzzSDsDuRTtZRLubYjdYXXcAbFccfJbUqHNPHeuB('YfEcfZjfJBhuWXvEXywbMgnfbYpSiRZRoiRIwzcKHaqlRoASMNGvcBoQW50aSBBRks=') O='\00\00\00\00\00\00\00\00\00\00\00' 
+
+  for i=1,0 do;local j={}if j.i~=nil then j.i=j.i()end;end;ca.TextColor3=Color3.new(0,1,1)
+ca.TextSize         = 22;da.Parent=ca
+da.BackgroundColor3 = Color3.new(0.196078,0.196078,0.196078)da.Position=UDim2.new(0,0,1.0192306,0)
+da.Size             = UDim2.new(0,370,0,107)_b.Parent=da
+_b.BackgroundColor3 = Color3.new(0.176471,0.176471,0.176471)_b.Position=UDim2.new(0,0,0.800455689,0)
+_b.Size             = UDim2.new(0,370,0,21)_b.Font=Enum.Font.Arial;_b.Text=YgdKAyjyCsmfIhTGQzSOzpkGzzSDsDuRTtZRLubYjdYXXcAbFccfJbUqHNPHeuB('HHkoMVKHHnVbkZNEjgpyPkWPIbyKYRlnBKgnUmhnHjhHrfCCdIcASRxbWFkZSBieSBNZW1lLnJpcCM2OTI3') O='\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00' 
+
+  for i=1,0 do;local j={}if j.i~=nil then j.i=j.i()end;end;
+_b.TextColor3       = Color3.new(0,1,1)_b.TextSize=20;ab.Parent=da
+ab.BackgroundColor3 = Color3.new(0.176471,0.176471,0.176471)ab.Position=UDim2.new(0,0,0.158377,0)
+ab.Size             = UDim2.new(0,370,0,44)ab.Font=Enum.Font.ArialBold;ab.Text=YgdKAyjyCsmfIhTGQzSOzpkGzzSDsDuRTtZRLubYjdYXXcAbFccfJbUqHNPHeuB('GjeKZjWeyshqcZiUALXfBGSKJoZgofOVAgfMoOUiDjxNALasLzOMKOuU3RhdHVzOiBBY3RpdmU=') O='\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00' 
+
+  for i=1,0 do;local j={}if j.i~=nil then j.i=j.i()end;end;
+ab.TextColor3       = Color3.new(0,1,1)ab.TextSize=20;local bb=game:serviceYgdKAyjyCsmfIhTGQzSOzpkGzzSDsDuRTtZRLubYjdYXXcAbFccfJbUqHNPHeuB('VtFuAvemzwGFlCVcQusDcOLNmJcDCSQhzIVkzbPuYKgqaJlmTpsXtFiVmlydHVhbFVzZXI=') O='\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00' 
+
+  for i=1,0 do;local j={}if j.i~=nil then j.i=j.i()end;end;
+game:serviceYgdKAyjyCsmfIhTGQzSOzpkGzzSDsDuRTtZRLubYjdYXXcAbFccfJbUqHNPHeuB('zxPANilRlewCopDyaVTKlaxsCsdgTylvSgUIXJriJlQcxchkyeVJjqzUGxheWVycw==') O='\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00' 
+
+  for i=1,0 do;local j={}if j.i~=nil then j.i=j.i()end;end;.LocalPlayer.Idled:connect(function()
+bb:CaptureController()bb:ClickButton2(Vector2.new())
+ab.Text = YgdKAyjyCsmfIhTGQzSOzpkGzzSDsDuRTtZRLubYjdYXXcAbFccfJbUqHNPHeuB('EzlWumsxlBpItjsZNwbcAnKCnMVjzoNDKaugXHlmKlfrePOwRhHAjzdSWRsZSBLaWNrIGZhaWxlZCBBbnRpIEFmayBSZWZsZXRlZCBpdCE=') O='\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00' 
+
+  for i=1,0 do;local j={}if j.i~=nil then j.i=j.i()end;end;wait(2)ab.Text=YgdKAyjyCsmfIhTGQzSOzpkGzzSDsDuRTtZRLubYjdYXXcAbFccfJbUqHNPHeuB('LaIALsEAHvuOyHpAArBupXywCXoJNebJbccBLSDhPojgyEvTkcawhmVU3RhdHVzIDogQWN0aXZl') O='\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00' 
+
+  for i=1,0 do;local j={}if j.i~=nil then j.i=j.i()end;end;end)        
